e31d2299f78194dfc2c39dbcf96a9791de8ef77a
madeinchema <chemavg1@gmail.com>
2020-09-06 22:58:46 +0200

    Added Todo component.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   CSSReset,
 } from "@chakra-ui/core";
 import Navbar from './components/Navbar';
+import Todo from './components/Todo';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <CSSReset />
         <Navbar />
         Hello world
+        <Todo />
       </ColorModeProvider>
     </ThemeProvider>
   );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,13 +3,13 @@ import {
   Button
 } from "@chakra-ui/core";
 
-const Navbar = (props) => {
+const Todo = (props) => {
   return (
     <header>
-      <Button>Navbar</Button>
+      <Button>Todo</Button>
     </header>
 
   )
 }
 
-export default Navbar;
+export default Todo;
