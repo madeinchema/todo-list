7d1677d0f683fd45d0454a5677e3dcce257ac97f
madeinchema <chemavg1@gmail.com>
2020-09-06 22:06:04 +0200

    Added components folder. Added Navbar component.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {
   ColorModeProvider,
   CSSReset,
 } from "@chakra-ui/core";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <ThemeProvider>
       <ColorModeProvider>
         <CSSReset />
+        <Navbar />
         Hello world
       </ColorModeProvider>
     </ThemeProvider>
