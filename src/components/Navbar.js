2b0388030be001ebe5a9cfa3c8fc378563bb479a
madeinchema <chemavg1@gmail.com>
2020-09-08 17:54:52 +0200

    Built Navbar, added dark/light theme support and button-switch.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,57 @@
 import React from "react";
+import { FaClipboardCheck } from 'react-icons/fa';
 import {
-  Button
-} from "@chakra-ui/core";
+  Text,
+  Link,
+  Flex,
+  useColorMode,
+} from '@chakra-ui/core';
 
-const Navbar = (props) => {
+function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const bgColor = { light: "gray.100", dark: "gray.900" }
+  const color = { light: "gray.900", dark: "gray.100" }
+
   return (
-    <header>
-      <Button>Navbar</Button>
-    </header>
+    <React.Fragment>
+      <Flex
+        bg={bgColor[colorMode]}
+        color={color[colorMode]}
+        w='100%'
+        h='68px'
+        justify='space-between'
+        align='center'
+        px='24px'
+      >
 
-  )
-}
+        {/* Logo */}
+        <Text fontSize='2xl' fontWeight='700' >
+          <Link href='#' display='flex' alignItems='center'>
+            <FaClipboardCheck style={{marginRight: '4px'}}/>Todolist
+          </Link>
+        </Text>
+
+        {/* Menu */}
+        <Flex>
+          <Link>
+            <Text
+              textAlign='center'
+              fontWeight='500'
+              mr='24px'
+              onClick={() => toggleColorMode()}
+            >
+              Switch theme
+            </Text>
+          </Link>
+          <Link>
+            <Text textAlign='center' fontWeight='500'>Settings</Text>
+          </Link>
+        </Flex>
+
+      </Flex>
+    </React.Fragment>
+  );
+};
 
 export default Navbar;
+
