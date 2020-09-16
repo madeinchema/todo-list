e45fe73001c8fe5fb8af8ed1f1101c2508cbdc60
madeinchema <chemavg1@gmail.com>
2020-09-16 10:12:59 +0200

    Now we handleChange every to-do checkbox state.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,12 +7,16 @@ import {
 } from '@chakra-ui/core';
 import TodoInput from './TodoInput';
 
-function Todo(todo) {
+/**
+ * To-do component template
+ */
+function Todo({ todo, handleChange }) {
   return (
-    <li
-      key={todo.id}
-    >
-      <Checkbox>
+    <li key={todo.id}>
+      <Checkbox
+        isChecked={todo.checked}
+        onChange={() => handleChange(todo.id)}
+      >
         <Text>
           {todo.title}
         </Text>
@@ -21,18 +25,37 @@ function Todo(todo) {
   );
 }
 
+/**
+ * Todo: Will only render TodoList
+ */
 export default class TodoList extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      todos: [],
+      todos: [
+        {
+          id: 229,
+          title: 'Practice React without hooks',
+          checked: false,
+          indent: 1,
+          priority: 4,
+        },
+        {
+          id: 1239,
+          title: 'Knowledge',
+          checked: true,
+          indent: 1,
+          priority: 4,
+        },
+      ],
     }
 
     this.addTodo = this.addTodo.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-
+  // Adds a new to-do object to the "todos" state
   addTodo(todo) {
     this.setState((prevState) => {
       return ({
@@ -40,6 +63,15 @@ export default class TodoList extends React.Component {
       })
     });
     setTimeout(() => console.log(this.state.todos), 100)
+  }
+
+  // Updates the state of a to-do's checkbox
+  handleChange(id) {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(
+        todo => todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    }));
   }
 
   render() {
@@ -51,11 +83,20 @@ export default class TodoList extends React.Component {
           </Flex>
         </Box>
 
-        <Box>
-          <ul>
-            {this.state.todos.map(todo => Todo(todo))}
-          </ul>
-        </Box>
+        <Flex align="center" justify="center" h="calc(100vh - 168px)" direction="column">
+          <Box className='custom-scroll' h='100%' w='100%' maxW='680px' px='32px' py='24px' bg='rgba(0,0,0,.1)'
+               overflow='auto'>
+            <Flex id='todoList' flexDir='column'>
+
+              <ul>
+                {this.state.todos.map(todo => (
+                  <Todo todo={todo} handleChange={this.handleChange} />
+                ))}
+              </ul>
+
+            </Flex>
+          </Box>
+        </Flex>
 
       </Flex>
     );
