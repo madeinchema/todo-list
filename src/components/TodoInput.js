19fb0d1660cc2d5fc13a2d651cf71734be941ccd
madeinchema <chemavg1@gmail.com>
2020-09-16 11:14:27 +0200

    Rewritten to a functional component.

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -4,39 +4,26 @@ import {
   Button,
   Input,
   InputRightElement,
-  InputGroup,
+  InputGroup, Flex,
 } from '@chakra-ui/core';
 
-
-export default class TodoInput extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { title: '' }
-  }
-
-  // To-do title controlled component
-  handleChange = (event) => {
-    this.setState({ title: event.target.value })
-  }
-
+export default function TodoInput({ title, addTodo, handleTitle }) {
   // Adds the new to-do to the TodoList's state
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    this.props.addTodo(this.todo(this.state.title));
-    this.setState({ title: '' });
+    addTodo(todo(title));
   }
 
   // Generates random IDs for the to-dos
-  newId = () => {
-    // todo pseudocode: while (newId in todoList) {generator}
+  const newId = () => {
+    // todo pseudocode: while (newId is already in todoList) { keep generating }
     return Math.floor(Math.random() * 10000);
   }
 
   // To-do object template
-  todo = (title) => {
+  const todo = (title) => {
     return ({
-      id: this.newId(),
+      id: newId(),
       title: title,
       checked: false,
       indent: 1,
@@ -44,39 +31,45 @@ export default class TodoInput extends React.Component {
     })
   }
 
-  render() {
-    return (
-      <Box
-        w='100%'
-        maxW='680px'
-        as='form'
-      >
-        <InputGroup size="md" shadow='lg'>
+  return (
 
-          <Input
-            h='3rem'
-            pr="7.5rem"
-            fontWeight='500'
-            type="text"
-            placeholder="Task title"
-            value={this.state.title}
-            onChange={this.handleChange}
-          />
+    <Flex flexDir='column'>
+      <Box minH='100px' p='24px'>
+        <Flex justifyContent='center'>
 
-          <InputRightElement w="7.5rem" h='100%' p='0.25rem'>
-            <Button
-              h='100%'
-              w="100%"
-              type='submit'
-              onClick={this.handleSubmit}
-            >
-              Add Task
-            </Button>
+          <Box
+            w='100%'
+            maxW='680px'
+            as='form'
+          >
+            <InputGroup size="md" shadow='lg'>
 
-          </InputRightElement>
+              <Input
+                h='3rem'
+                pr="7.5rem"
+                fontWeight='500'
+                type="text"
+                placeholder="Task title"
+                value={title}
+                onChange={handleTitle}
+              />
 
-        </InputGroup>
+              <InputRightElement w="7.5rem" h='100%' p='0.25rem'>
+                <Button
+                  h='100%'
+                  w="100%"
+                  type='submit'
+                  onClick={handleSubmit}
+                >
+                  Add Task
+                </Button>
+
+              </InputRightElement>
+
+            </InputGroup>
+          </Box>
+        </Flex>
       </Box>
-    );
-  }
+    </Flex>
+  );
 };
