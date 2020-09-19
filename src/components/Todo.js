8184b98b247c49df6415591650eac72ae11356ba
madeinchema <chemavg1@gmail.com>
2020-09-19 18:34:44 +0200

    Added Hover functionality: to render TodoActions.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,44 +1,52 @@
 import React from 'react';
-import { Checkbox, Text, Flex, useColorMode } from '@chakra-ui/core';
+import { Checkbox, Text, Flex, useColorMode, Box } from '@chakra-ui/core';
 import PropTypes from 'prop-types';
+import Hover from './Hover';
+import TodoActions from './TodoActions';
 
 export default function Todo({ todo, handleChange }) {
-  const bgColor = { light: 'gray.50', dark: 'gray.700' }
+  const bgColor = { light: 'gray.50', dark: 'gray.800' }
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <li>
-      <Flex
-        h='auto'
-        py='.5rem'
-        px='.75rem'
-        align='flex-start'
-        mb='.25rem'
-        bg={bgColor[colorMode]}
-        shadow='md'
-        borderRadius='3px'
-      >
-        <Checkbox
-          my='.25rem'
-          size='lg'
-          isChecked={todo.checked}
-          onChange={() => handleChange(todo.id)}
-          d='flex'
-
-        >
-        </Checkbox>
-        <Text
-          mb='.25rem'
-          pl='.75rem'
-          fontSize='1.2em'
-          fontWeight='600'
-          lineHeight='1.5rem'
-          opacity={todo.checked ? '0.5' : '1'}
-        >
-          {todo.title}
-        </Text>
-      </Flex>
-    </li>
+    <Hover>
+      {(hovering) => (
+          <li>
+            <Flex
+              h='auto'
+              py='.5rem'
+              px='.75rem'
+              align='flex-start'
+              mb='.25rem'
+              bg={bgColor[colorMode]}
+              shadow='md'
+              borderRadius='3px'
+            >
+              <Checkbox
+                my='.25rem'
+                size='lg'
+                isChecked={todo.checked}
+                onChange={() => handleChange(todo.id)}
+                d='flex'
+              >
+              </Checkbox>
+              <Text
+                mt='.05rem'
+                pl='.75rem'
+                fontSize='1.2em'
+                fontWeight='600'
+                lineHeight='1.5rem'
+                opacity={todo.checked ? '0.5' : '1'}
+              >
+                {todo.title}
+              </Text>
+              <Box ml='auto' my='auto'>
+                {hovering && <TodoActions />}
+              </Box>
+            </Flex>
+          </li>
+      )}
+    </Hover>
   );
 }
 
