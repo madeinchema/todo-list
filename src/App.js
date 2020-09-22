6cce8db729d8d1cbd29fda132482d31d5c3baab7
madeinchema <chemavg1@gmail.com>
2020-09-22 21:36:09 +0200

    From class comp. to func. comp. with hooks.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   ThemeProvider,
   ColorModeProvider,
@@ -10,96 +10,86 @@ import Navbar from './components/Navbar';
 import TodoList from './components/TodoList';
 import TodoInput from './components/TodoInput';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      inputTitle: '',
-      lastTitle: '',
-      todos: [],
-    }
-  }
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputTitle, setInputTitle] = useState('');
+  const [lastTitle, setLastTitle] = useState('');
 
   // Set todos state if there are todos saved in localStorage
-  componentDidMount() {
+  useEffect(() => {
     if (JSON.parse(localStorage.getItem('todos')) !== null) {
-      this.setState({ todos: JSON.parse(localStorage.getItem('todos')) });
+      setTodos(JSON.parse(localStorage.getItem('todos')));
     }
-  }
+  }, [])
 
   // Update localStorage todos to match the current state
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    this.setLocal('todos', this.state.todos);
-  }
+  useEffect(() => {
+    setLocal('todos', todos);
+  }, [todos])
 
   // Sets the value for the localStorage key passed
-  setLocal = (key, value) => {
+  const setLocal = (key, value) => {
     localStorage.setItem(key, JSON.stringify(value));
   }
 
   // Task title input value handler for controlled component
-  handleTitle = (event) => {
-    this.setState({ inputTitle: event.target.value })
+  const handleTitle = (event) => {
+    setInputTitle(event.target.value);
   }
 
   // Adds a new to-do object to the "todos" state
-  addTodo = (todo) => {
-    this.setState((prevState) => ({ todos: [...prevState.todos, todo] }));
-    this.setState({ inputTitle: '' });
+  const addTodo = (todo) => {
+    setTodos((prevState) => [...prevState, todo]);
+    setInputTitle('')
   }
 
-  editTodo = (event, id, lastTitle) => {
+  const editTodo = (event, id, lastTitle) => {
     const currentChange = lastTitle ? lastTitle : event.target.value;
-    this.setState(prevState => ({
-      todos: prevState.todos.map(
+
+    setTodos(prevState => {
+      return prevState.map(
         todo => todo.id === id
           ? { ...todo, title: currentChange }
           : todo
       )
-    }));
-  }
-
-  removeTodo = (id) => {
-    this.setState(prevState => {
-      const todos = prevState.todos.filter(todo => todo.id !== id);
-      return { todos }
     })
   }
 
-  // Updates the state of a to-do's checkbox
-  handleChange = (id) => {
-    this.setState(prevState => ({
-      todos: prevState.todos.map(
-        todo => todo.id === id ? { ...todo, checked: !todo.checked } : todo
-      )
-    }));
+  const removeTodo = (id) => {
+    setTodos(prevState => prevState.filter(todo => todo.id !== id));
   }
 
-  render() {
-    return (
-      <ThemeProvider>
-        <ColorModeProvider>
-          <TurnOnColorMode>
-            <CSSReset />
-            <Navbar />
-            <TodoInput
-              title={this.state.inputTitle}
-              addTodo={this.addTodo}
-              handleTitle={this.handleTitle}
-            />
-            <TodoList
-              todos={this.state.todos}
-              handleChange={this.handleChange}
-              editTodo={this.editTodo}
-              removeTodo={this.removeTodo}
-            />
-          </TurnOnColorMode>
-        </ColorModeProvider>
-      </ThemeProvider>
-    );
+  // Updates the state of a to-do's checkbox
+  const handleChange = (id) => {
+    setTodos(prevState => (
+      prevState.map(todo => todo.id === id
+        ? { ...todo, checked: !todo.checked }
+        : todo
+    )))
   }
-}
+
+  return (
+    <ThemeProvider>
+      <ColorModeProvider>
+        <TurnOnColorMode>
+          <CSSReset />
+          <Navbar />
+          <TodoInput
+            title={inputTitle}
+            addTodo={addTodo}
+            handleTitle={handleTitle}
+          />
+          <TodoList
+            todos={todos}
+            handleChange={handleChange}
+            editTodo={editTodo}
+            removeTodo={removeTodo}
+          />
+        </TurnOnColorMode>
+      </ColorModeProvider>
+    </ThemeProvider>
+  );
+};
 
 // ColorMode for the App's background
 function TurnOnColorMode({ children }) {
