d977c02ad988a45c4240eae5238f866a57b753e6
madeinchema <chemavg1@gmail.com>
2020-09-23 11:03:50 +0200

    Imported TodoContext in App. Moved useEffects to TodoContext.

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,9 +1,21 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 
 const TodoContext = createContext();
 
 const TodoContextProvider = (props) => {
   const [todos, setTodos] = useState([]);
+
+  // Set todos state if there are todos saved in localStorage
+  useEffect(() => {
+    if (JSON.parse(localStorage.getItem('todos')) !== null) {
+      setTodos(JSON.parse(localStorage.getItem('todos')));
+    }
+  }, [])
+
+  // Update localStorage todos to match the current state
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos])
 
   return (
     <TodoContext.Provider value={{ todos, setTodos }}>
