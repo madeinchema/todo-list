3407361fc4872693a23c7faba897f13829188631
madeinchema <chemavg1@gmail.com>
2020-09-29 18:32:16 +0200

    Updated localStorage and new data structure handling during the app's initialization.

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -1,22 +1,35 @@
 import React, { useReducer, useState, createContext, useEffect } from 'react';
 // import { TodoReducer } from '../reducers/TodoReducer';
-import initialData from '../initial-data';
+// import initialData from '../initial-data';
 
 const TodoContext = createContext();
 
+const initialData = {
+  tasks: {},
+  columns: {
+    'column-1': {
+      id: 'column-1',
+      title: 'To do',
+      taskIds: [],
+    }
+  },
+  // Facilitate reordering of the columns
+  columnOrder: ['column-1']
+};
+
 const TodoContextProvider = (props) => {
-  // const [todosData, dispatch] = useReducer(TodoReducer, {}, () => {
-  const [todosData, setTodosData] = useState({ ...initialData
-    // // Set todos state if there are todos saved in localStorage
-    // const localData = localStorage.getItem('todos');
-    // return localData !== null ? JSON.parse(localData) : { ...initialData };
-    // return { ...initialData };
-  });
+  const [todosData, setTodosData] = useState(initialData);
+
+  // Initialize app checking and update todosData if it's set in localStorage
+  useEffect(() => {
+    if (localStorage.getItem('todos-v1') !== null) {
+      setTodosData(JSON.parse(localStorage.getItem('todos-v1')));
+    }
+  }, [])
 
   // Update localStorage todos to match the current state
   useEffect(() => {
-    console.log('useEffect in TodoContext. todosData:', todosData)
-    // localStorage.setItem('todos', JSON.stringify(todosData));
+    localStorage.setItem('todos-v1', JSON.stringify(todosData));
   }, [todosData])
 
   return (
