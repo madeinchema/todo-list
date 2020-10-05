f9ef176ddff93c44a463a3ed94f84d0f78de2f43
madeinchema <chemavg1@gmail.com>
2020-10-05 21:27:59 +0200

    The icon is now a menu that opens a list of actions.

--- src/components/TodoActions.js
+++ src/components/TodoActions.js
@@ -1,7 +1,16 @@
 import React, { useContext } from 'react';
-import { Link, PseudoBox, Icon } from '@chakra-ui/core';
+import {
+  Flex,
+  PseudoBox,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from '@chakra-ui/core';
 import PropTypes from 'prop-types';
 import { TodoContext } from '../contexts/TodoContext';
+import {MdMoreVert} from 'react-icons/all';
 
 export default function TodoActions({ todo, index }) {
   const { dispatch } = useContext(TodoContext);
@@ -16,23 +25,29 @@ export default function TodoActions({ todo, index }) {
   }
 
   return (
-    <Link
-      onClick={removeTodo}
-    >
-      <PseudoBox
-        style={{ transition: 'all .1s ease-out' }}
-        d='flex'
-        opacity='0.35'
-        _hover={{ opacity: "1" }}
-      >
-        <Icon
-          alignSelf='center'
-          aria-label="Remove to-do"
-          name="delete"
-          size='1rem'
-        />
-      </PseudoBox>
-    </Link>
+    <Flex>
+
+        <Menu>
+          <PseudoBox
+            style={{ transition: 'all .1s ease-out' }}
+            d='flex'
+            opacity='0.5'
+            _hover={{ opacity: "1" }}
+          >
+            <MenuButton aria-label={'Open todo menu'}>
+              <Icon aria-label="Search database" as={MdMoreVert} size='1.5rem' />
+            </MenuButton>
+          </PseudoBox>
+
+          <MenuList>
+            <MenuItem onClick={removeTodo}>
+              <Icon aria-label="Search database" name='delete' size='1rem' mr='.5rem' opacity='.75'/>Delete
+            </MenuItem>
+
+          </MenuList>
+        </Menu>
+
+    </Flex>
   );
 }
 
