118f9be0fb115b7f9503ba09bfce3fdcf80a9d5c
madeinchema <chemavg1@gmail.com>
2020-10-12 21:14:17 +0200

    Added badge to show the number of tasks inside a column.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,31 +1,36 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import { Box, Flex, Text, List, Heading, Icon } from '@chakra-ui/core';
+import { Box, Flex, Text, List, Heading, Icon, Badge } from '@chakra-ui/core';
 import Task from './Task';
 import { TasksContext } from '../contexts/TasksContext';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import { MdCheck, MdSort } from 'react-icons/all';
 
-const ColumnHeader = ({ title }) => (
-  <Flex
-    mb='.5rem'
-    w='100%'
-    justify='space-between'
-    align='flex-end'
-  >
-    <Flex align='center'>
-      <Icon name='chevron-right' size='1.5rem' mt='.25rem'/>
-      <Heading size='lg'>{title}</Heading>
+const ColumnHeader = ({ title, quantity }) => {
+
+  return (
+    <Flex
+      mb='.5rem'
+      w='100%'
+      justify='space-between'
+      align='flex-end'
+    >
+      <Flex align='center'>
+        <Icon name='chevron-right' size='1.5rem' mt='.25rem'/>
+        <Heading size='lg' mr='.5rem'>{title}</Heading>
+        <Badge mt='.2rem' fontSize='.8rem'>{quantity}</Badge>
+      </Flex>
+      <Flex align='center' pr='.75rem'>
+        <Icon as={MdSort} size='1.5rem' mr='.25rem'/>
+        <Text display='inline-block' fontWeight='500'>Sort</Text>
+      </Flex>
     </Flex>
-    <Flex align='center' pr='.75rem'>
-      <Icon as={MdSort} size='1.5rem' mr='.25rem'/>
-      <Text display='inline-block' fontWeight='500'>Sort</Text>
-    </Flex>
-  </Flex>
-)
+  )
+}
 
 export default function TaskList() {
   const { tasksData, dispatch } = useContext(TasksContext);
+  const tasksLength = tasksData.columns['column-1'].taskIds.length;
 
   // Handle the dropping of tasks
   const onDragEnd = result => {
@@ -37,28 +42,28 @@ export default function TaskList() {
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <Flex
-        direction="column"
-        w='100%'
-        h="calc(100vh - 178px)"
-      >
-        <ColumnHeader title={'To do'}/>
-        <Box
-          className='custom-scroll'
-          overflow='auto'
-          borderRadius='5px'
-          px='.5rem'
+
+      {tasksData.columns['column-1'].taskIds.length === 0 && (
+        <Flex justify='center' align='center' height='40vh' direction='column'>
+          <Icon as={MdCheck} size='4rem'/>
+          <Heading size='lg'>There are no tasks</Heading>
+        </Flex>
+      )}
+
+      {tasksData.columns['column-1'].taskIds.length >= 1 && (
+        <Flex
+          direction="column"
+          w='100%'
+          h="calc(100vh - 178px)"
         >
-          <Flex flexDir='column'>
-
-            {tasksData.columns['column-1'].taskIds.length === 0 && (
-              <Flex justify='center' align='center' height='40vh' direction='column'>
-                <Icon as={MdCheck} size='4rem' />
-                <Heading size='lg'>There are no tasks</Heading>
-              </Flex>
-            )}
-
-            {tasksData.columns['column-1'].taskIds.length >= 1 && (
+          <ColumnHeader title={'To do'} quantity={tasksLength}/>
+          <Box
+            className='custom-scroll'
+            overflow='auto'
+            borderRadius='5px'
+            px='.5rem'
+          >
+            <Flex flexDir='column'>
               <List mb='2rem'>
                 {tasksData && tasksData.columnOrder.map((columnId) => {
                   const column = tasksData.columns[columnId];
@@ -78,13 +83,14 @@ export default function TaskList() {
                         </Box>
                       )}
                     </Droppable>
-                  );
+                  )
                 })}
               </List>
-            )}
-          </Flex>
-        </Box>
-      </Flex>
+            </Flex>
+          </Box>
+        </Flex>
+      )}
+
     </DragDropContext>
   );
 };
