a82d1eeda6349f75446fb823a059e4e2a0ad3136
madeinchema <chemavg1@gmail.com>
2020-10-16 22:26:03 +0200

    Added Heading and Button to go back.

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Box, Flex, Text, Checkbox } from '@chakra-ui/core';
+import { Box, Flex, Text, Button, Checkbox, Icon, Heading } from '@chakra-ui/core';
+import { MdArrowBack} from 'react-icons/all';
 
 const Settings = () => {
   return (
-    <Box>
-      Settings
+    <Box mx='.5rem' mb='5rem'>
+      <Flex w='100%' justify='space-between' mb='1rem'>
+        <Heading size='lg'>Settings</Heading>
+        <Button size='sm'>
+          <Icon as={MdArrowBack} mr='.25rem'/>
+          <Text mb='.05rem'>Go back</Text>
+        </Button>
+      </Flex>
     </Box>
   );
 };
