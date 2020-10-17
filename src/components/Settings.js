1d8ce5115ee901164e06524ae6c5cd65e09fb830
madeinchema <chemavg1@gmail.com>
2020-10-17 22:38:57 +0200

    Improved Settings container and heading section styles.

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Box, Flex, Text, Button, Checkbox, Icon, Heading } from '@chakra-ui/core';
+import { Box, Flex, Text, Link, Button, Switch, Icon, Heading } from '@chakra-ui/core';
 import { MdArrowBack} from 'react-icons/all';
 
 const Settings = () => {
   return (
-    <Box mx='.5rem' mb='5rem'>
-      <Flex w='100%' justify='space-between' mb='1rem'>
-        <Heading size='lg'>Settings</Heading>
-        <Button size='sm'>
-          <Icon as={MdArrowBack} mr='.25rem'/>
-          <Text mb='.05rem'>Go back</Text>
-        </Button>
+    <Box maxW='384px' w='100%' mx='auto' mb='5rem' p='1rem'>
+      <Flex w='100%' justify='space-between' align='center' mb='1.5rem'>
+
+        <Box flexGrow='1' width='100%'>
+          <Button size='sm' as={Link} href='/'>
+            <Icon as={MdArrowBack} mr='.25rem'/>
+            <Text mb='.05rem'>Go back</Text>
+          </Button>
+        </Box>
+
+        <Box>
+          <Heading size='lg' mb='.125rem'>Settings</Heading>
+        </Box>
+
+        <Box flexGrow='1' width='100%'/>
       </Flex>
     </Box>
   );
