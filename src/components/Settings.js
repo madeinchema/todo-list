f260a5d57647e2e09e896062bdfa53bf6caef322
madeinchema <chemavg1@gmail.com>
2020-10-18 22:12:54 +0200

    Added Router Link to go back. Minor styling improvements.

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Box, Flex, Text, Link, Button, Switch, Icon, Heading } from '@chakra-ui/core';
+import { Link as ReactLink } from 'react-router-dom';
+import { Box, Flex, Text, Button, Switch, Icon, Heading } from '@chakra-ui/core';
 import { MdArrowBack} from 'react-icons/all';
 
 const Settings = () => {
   return (
-    <Box maxW='384px' w='100%' mx='auto' mb='5rem' p='1rem'>
+    <Box
+      mx='auto'
+      mb='5rem'
+      p='2rem 1rem'
+      maxW='512px'
+      w='100%'
+      h="calc(100vh - 72px)"
+    >
       <Flex w='100%' justify='space-between' align='center' mb='1.5rem'>
 
         <Box flexGrow='1' width='100%'>
-          <Button size='sm' as={Link} href='/'>
+          <Button size='sm' as={ReactLink} to='/'>
             <Icon as={MdArrowBack} mr='.25rem'/>
             <Text mb='.05rem'>Go back</Text>
           </Button>
@@ -25,7 +33,7 @@ const Settings = () => {
         <Heading size='mg' mb='.75rem'>Notes and Lists</Heading>
         <Flex justify='space-between' align='center' mb='.75rem'>
           <Text>Move completed tasks to the bottom</Text>
-          <Switch size="sm" />
+          <Switch size="sm" isDisabled={true}/>
         </Flex>
       </Flex>
     </Box>
