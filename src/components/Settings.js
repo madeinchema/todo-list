5906bff491d35db59b202e9529b4e59c9aab75d2
madeinchema <chemavg1@gmail.com>
2020-10-19 10:57:19 +0200

    Added divider and deleteAll themed button.

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Link as ReactLink } from 'react-router-dom';
-import { Box, Flex, Text, Button, Switch, Icon, Heading } from '@chakra-ui/core';
+import { Box, Flex, Text, Button, Switch, Icon, Heading, Divider, useColorMode } from '@chakra-ui/core';
 import { MdArrowBack} from 'react-icons/all';
 
 const Settings = () => {
+  const { colorMode } = useColorMode();
+  const deleteBtnColor = { light: 'red.600', dark: 'red.400' };
+  const dividerColor = { light: 'gray.400', dark: 'gray.600' };
+
+
   return (
     <Box
       mx='auto'
@@ -12,8 +17,7 @@ const Settings = () => {
       maxW='512px'
       w='100%'
     >
-      <Flex w='100%' justify='space-between' align='center' mb='1.5rem'>
-
+      <Flex w='100%' justify='space-between' align='center' my='1.5rem'>
         <Box flexGrow='1' width='100%'>
           <Button size='sm' as={ReactLink} to='/'>
             <Icon as={MdArrowBack} mr='.25rem'/>
@@ -33,6 +37,15 @@ const Settings = () => {
         <Flex justify='space-between' align='center' mb='.75rem'>
           <Text>Move completed tasks to the bottom</Text>
           <Switch size="sm" isDisabled={true}/>
+        </Flex>
+        <Divider borderColor={dividerColor[colorMode]}/>
+        <Flex align='center' my='.75rem'>
+          <Button
+            borderColor={deleteBtnColor[colorMode]}
+            color={deleteBtnColor[colorMode]}
+            variant='outline'
+            variantColor='red'
+          >Delete all the tasks</Button>
         </Flex>
       </Flex>
     </Box>
