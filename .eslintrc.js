1672ee3ddc57ce8c9d431a92de6c363bf6896f1f
madeinchema <chemavg1@gmail.com>
2020-12-06 23:36:07 +0100

    install eslint plugins, add .prettierrc and .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+  ],
+  plugins: ['prettier', 'import', 'react-hooks', 'jsx-a11y'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx'],
+  },
+};
