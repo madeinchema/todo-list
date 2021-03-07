9f8635f6b00ae7bd327256088139b45051015472
madeinchema <chemavg1@gmail.com>
2021-03-07 22:23:54 +0100

    use redux Provider in index.jsx & pass the store

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ChakraProvider, useColorMode, Box } from '@chakra-ui/react';
@@ -6,29 +7,32 @@ import Header from './components/Header/Header';
 import TasksList from './pages/TasksList/TasksList';
 import { TasksContextProvider } from './contexts/TasksContext';
 import Settings from './pages/Settings/Settings';
+import store from './redux/store';
 import './index.scss';
 
 const App = () => {
   return (
-    <Router>
-      <ChakraProvider>
-        <TurnOnColorMode>
-          <TasksContextProvider>
-            <Header />
+    <Provider store={store}>
+      <Router>
+        <ChakraProvider>
+          <TurnOnColorMode>
+            <TasksContextProvider>
+              <Header />
 
-            <Switch>
-              <Route exact path="/">
-                <TasksList columnId="to-do" />
-              </Route>
+              <Switch>
+                <Route exact path="/">
+                  <TasksList columnId="to-do" />
+                </Route>
 
-              <Route path="/settings">
-                <Settings />
-              </Route>
-            </Switch>
-          </TasksContextProvider>
-        </TurnOnColorMode>
-      </ChakraProvider>
-    </Router>
+                <Route path="/settings">
+                  <Settings />
+                </Route>
+              </Switch>
+            </TasksContextProvider>
+          </TurnOnColorMode>
+        </ChakraProvider>
+      </Router>
+    </Provider>
   );
 };
 
