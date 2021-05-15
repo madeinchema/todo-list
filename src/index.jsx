ad681adbb80043a200668010bd7df10ebfbd31b6
madeinchema <chemavg1@gmail.com>
2021-05-15 18:57:13 +0200

    remove TasksContextProvider from index

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { ChakraProvider, useColorMode, Box } from '@chakra-ui/react'
 import Header from './components/Header/Header'
 import TasksList from './pages/TasksList/TasksList'
-import { TasksContextProvider } from './contexts/TasksContext'
 import Settings from './pages/Settings/Settings'
 import store from './redux/store'
 import './index.scss'
@@ -16,19 +15,17 @@ const App = () => {
       <Router>
         <ChakraProvider>
           <TurnOnColorMode>
-            <TasksContextProvider>
-              <Header />
+            <Header />
 
-              <Switch>
-                <Route exact path="/">
-                  <TasksList columnId="to-do" />
-                </Route>
+            <Switch>
+              <Route exact path="/">
+                <TasksList columnId="to-do" />
+              </Route>
 
-                <Route path="/settings">
-                  <Settings />
-                </Route>
-              </Switch>
-            </TasksContextProvider>
+              <Route path="/settings">
+                <Settings />
+              </Route>
+            </Switch>
           </TurnOnColorMode>
         </ChakraProvider>
       </Router>
