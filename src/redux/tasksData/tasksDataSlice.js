b615581729fbc329d1852fa576a628c925763dfe
madeinchema <chemavg1@gmail.com>
2021-05-16 17:47:29 +0200

    add & use initialTasksData const & clean slice code

--- src/redux/tasksData/tasksDataSlice.js
+++ src/redux/tasksData/tasksDataSlice.js
@@ -1,50 +1,11 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
 
-const defaultData = {
-  tasks: {
-    // 'task-1': { id: 'task-1', title: 'Take out the garbage', checked: true, priority: 1 },
-    // 'task-2': { id: 'task-2', title: 'Watch my favorite show', checked: false, priority: 2 },
-    // 'task-3': { id: 'task-3', title: 'Charge my phone', checked: false, priority: 3 },
-    // 'task-4': { id: 'task-4', title: 'Cook dinner', checked: false, priority: 4 },
-  },
-  columns: {
-    'to-do': {
-      id: 'to-do',
-      title: 'To do',
-      taskIds: [
-        // 'task-1', 'task-2', 'task-3', 'task-4'
-      ],
-    },
-  },
-  // Facilitate reordering of the columns
-  columnOrder: ['to-do'],
-}
+import initialTasksData from '../../utils/constants/initialTasksData'
 
 const persistedState = localStorage.getItem('tasks-v1')
   ? { ...JSON.parse(localStorage.getItem('tasks-v1')), settings: undefined }
-  : defaultData
+  : initialTasksData.empty
 
-// export const saveState = (state) => {
-//   try {
-//     const serializedState = JSON.stringify(state);
-//     localStorage.setItem('state', serializedState);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
-// export const loadState = () => {
-//   try {
-//     const serializedState = localStorage.getItem('state');
-//     if (!serializedState) return defaultData;
-//     return JSON.parse(serializedState);
-//   } catch (err) {
-//     saveState(defaultData);
-//     return undefined;
-//   }
-// };
-
-// const persistedStore = loadState();
 export const tasksDataSlice = createSlice({
   name: 'tasksData',
   initialState: persistedState,
@@ -59,7 +20,7 @@ export const tasksDataSlice = createSlice({
 
       if (!isManualSort || !destinationExists || isSameDestination) return state
 
-      // Reorder the taskIds, remove & add item from old to new index
+      // Reorder; remove & add item from old to new index
       state.columns[source.droppableId].taskIds.splice(source.index, 1)
       state.columns[source.droppableId].taskIds.splice(
         destination.index,
