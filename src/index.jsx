9f4e3979daf8ef243a5e48a1ff4441b5bed66f89
madeinchema <chemavg1@gmail.com>
2021-05-17 09:50:54 +0200

    add customTheme to Chakra & remove TurnOnColorMode func

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Box } from '@chakra-ui/layout'
-import { useColorMode } from '@chakra-ui/color-mode'
 import { ChakraProvider } from '@chakra-ui/react'
+import customTheme from './theme'
 
 import Header from './components/Header/Header'
 import TasksList from './pages/TasksList/TasksList'
@@ -16,32 +15,22 @@ const App = () => {
   return (
     <Provider store={store}>
       <Router>
-        <ChakraProvider>
-          <TurnOnColorMode>
-            <Header />
+        <ChakraProvider theme={customTheme}>
+          <Header />
 
-            <Switch>
-              <Route exact path="/">
-                <TasksList columnId="to-do" />
-              </Route>
+          <Switch>
+            <Route exact path="/">
+              <TasksList columnId="to-do" />
+            </Route>
 
-              <Route path="/settings">
-                <Settings />
-              </Route>
-            </Switch>
-          </TurnOnColorMode>
+            <Route path="/settings">
+              <Settings />
+            </Route>
+          </Switch>
         </ChakraProvider>
       </Router>
     </Provider>
   )
-}
-
-// ColorMode for the App's background
-function TurnOnColorMode({ children }) {
-  const bgColor = { light: 'gray.100', dark: 'gray.900' }
-  const { colorMode } = useColorMode()
-
-  return <Box bg={bgColor[colorMode]}>{children}</Box>
 }
 
 ReactDOM.render(
