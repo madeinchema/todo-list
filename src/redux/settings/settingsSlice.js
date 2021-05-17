5bb3f51fcfda258d627efad6bf3befbbf832440f
madeinchema <chemavg1@gmail.com>
2021-05-17 13:56:36 +0200

    add filter to state & setTasksFilter reducer

--- src/redux/settings/settingsSlice.js
+++ src/redux/settings/settingsSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 const defaultData = {
   moveCompletedTasksToBottom: false,
   sort: 'MANUAL',
+  filter: 'ALL',
 }
 
 const persistedState = localStorage.getItem('tasks-v1')
@@ -17,13 +18,19 @@ export const settingsSlice = createSlice({
     setTasksSort: (state, { payload: { order } }) => {
       state.sort = order
     },
+    setTasksFilter(state, { payload: { filter } }) {
+      state.filter = filter
+    },
     toggleMoveCompletedTasksToBottom(state) {
       state.moveCompletedTasksToBottom = !state.moveCompletedTasksToBottom
     },
   },
 })
 
-export const { setTasksSort, toggleMoveCompletedTasksToBottom } =
-  settingsSlice.actions
+export const {
+  setTasksSort,
+  setTasksFilter,
+  toggleMoveCompletedTasksToBottom,
+} = settingsSlice.actions
 
 export default settingsSlice.reducer
