6d820de732cd92ed0e5de9a3b4638f45ee6537d0
madeinchema <chemavg1@gmail.com>
2021-05-18 09:57:11 +0200

    refactor TaskList to take 100% of remaining height

--- src/pages/TasksList/TasksList.jsx
+++ src/pages/TasksList/TasksList.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useMemo } from 'react'
 import PropTypes from 'prop-types'
-import { Box, Flex, List } from '@chakra-ui/layout'
+import { Flex, List } from '@chakra-ui/layout'
 import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 import { useDispatch, useSelector } from 'react-redux'
 import { handleDragEnd } from '../../redux/tasksData/tasksDataSlice'
 import { getPreparedTasksIds } from './utils/functions/getPreparedTasksIds'
 
 import TaskItem from '../../components/TaskItem/TaskItem'
-import NewTask from './components/NewTask/NewTask'
 import EmptyTasksList from './components/EmptyTasksList'
 import TasksListMenu from './components/TasksListMenu/TasksListMenu'
 import useLocalStorage from '../../hooks/useLocalStorage'
@@ -47,57 +46,46 @@ const TasksList = ({ columnId }) => {
     )
 
   return (
-    <Flex direction="column" maxW="680px" mx="auto">
-      <NewTask />
-      <DragDropContext onDragEnd={onDragEnd}>
-        {columns[columnId].taskIds.length >= 1 ? (
-          <Box h="calc(100vh - 4.5rem)">
-            <TasksListMenu quantity={tasksQty} columnId={columnId} />
-            <Flex
-              flexDir="column"
-              className="custom-scroll"
-              overflow="auto"
-              borderRadius="5px"
-              px=".5rem"
-              h="calc(100vh - 13.25rem)"
-            >
-              <List mb="2rem">
-                <Droppable droppableId="to-do" key={column.id} tasks={tasks}>
-                  {({ innerRef, droppableProps, placeholder }, snapshot) => (
-                    <Box
-                      ref={innerRef}
-                      data-rbd-droppable-context-id={
-                        droppableProps['data-rbd-droppable-context-id']
-                      }
-                      data-rbd-droppable-id={
-                        droppableProps['data-rbd-droppable-id']
-                      }
-                      isdraggingover={snapshot.isDraggingOver.toString()}
-                    >
-                      {preparedTasksIds.map((taskId, index) => {
-                        const task = tasks[taskId]
-                        return (
-                          <TaskItem
-                            key={taskId}
-                            task={task}
-                            index={index}
-                            droppableSnapshot={snapshot}
-                            columnId={column.id}
-                          />
-                        )
-                      })}
-                      {placeholder}
-                    </Box>
-                  )}
-                </Droppable>
+    <DragDropContext onDragEnd={onDragEnd}>
+      {columns[columnId].taskIds.length >= 1 ? (
+        <Flex flexDirection="column" h="100%">
+          <TasksListMenu quantity={tasksQty} columnId={columnId} />
+          <Droppable droppableId="to-do" key={column.id} tasks={tasks}>
+            {({ innerRef, droppableProps, placeholder }, snapshot) => (
+              <List
+                ref={innerRef}
+                data-rbd-droppable-context-id={
+                  droppableProps['data-rbd-droppable-context-id']
+                }
+                data-rbd-droppable-id={droppableProps['data-rbd-droppable-id']}
+                isdraggingover={snapshot.isDraggingOver.toString()}
+                className="custom-scroll"
+                overflow="auto"
+                px=".5rem"
+                pb={4}
+                flex="1 1 100px"
+              >
+                {preparedTasksIds.map((taskId, index) => {
+                  const task = tasks[taskId]
+                  return (
+                    <TaskItem
+                      key={taskId}
+                      task={task}
+                      index={index}
+                      droppableSnapshot={snapshot}
+                      columnId={column.id}
+                    />
+                  )
+                })}
+                {placeholder}
               </List>
-            </Flex>
-          </Box>
-        ) : (
-          <EmptyTasksList />
-        )}
-      </DragDropContext>
-    </Flex>
+            )}
+          </Droppable>
+        </Flex>
+      ) : (
+        <EmptyTasksList />
+      )}
+    </DragDropContext>
   )
 }
 
